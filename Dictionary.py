9e05a942d557491d7095ac543ecdbe0079dbd60d
Jefferson Dias <diasjefferson2@gmail.com>
2022-02-25 10:04:57 -0500

    Code for New file containing 5 letter words

--- Dictionary.py
+++ Dictionary.py
@@ -5,13 +5,18 @@ import random
 
 def getRandomWord():
     f = open("words.txt", "r")
+    new_file = open("new_words.txt", "w")
     myWordList = []
     for x in f:
         if(len(x) == 6):  # words contain '\n' at the end which counts as 1 character, hence 6
             myWordList.append(x[:-1].upper())
+            # writing to new file "new_file.txt"
+            new_file.write(f"{x.upper()}")
 
     word = random.choices(myWordList)
     for item in word:
         finalWord = item
 
+    f.close()
+    new_file.close()
     return finalWord.upper(), myWordList
