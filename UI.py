c544ea690b002be0c5eb4b8e87e6d0cf858955b2
Jefferson Dias <diasjefferson2@gmail.com>
2022-03-20 00:03:59 -0400

    Assignment-8

--- UI.py
+++ UI.py
@@ -2,36 +2,35 @@
 from typing import List
 
 
-def inputFromUser(attempt: int) -> str:
-    input_word = input(
-        f'\nAttempt {attempt+1} - Please enter a 5 character word : ').upper()
-    return input_word
+class Ui:
+    def inputFromUser(attempt: int) -> str:
+        input_word = input(
+            f'\nAttempt {attempt+1} - Please enter a 5 character word : ').upper()
+        return input_word
 
-# check if entered word is having any special characters
+    # check if entered word is having any special characters
 
+    def checkInput(input_word: str) -> bool:
+        if input_word.isalpha() == False:
+            print('\nWARNING : Please enter a word containing only Alphabets.')
+            return False
+        else:
+            return True
 
-def checkInput(input_word: str) -> bool:
-    if input_word.isalpha() == False:
-        print('\nWARNING : Please enter a word containing only Alphabets.')
-        return False
-    else:
-        return True
+    # check if entered word has already been entered before
 
-# check if entered word has already been entered before
+    def checkWordUsed(input_word: str, entered_words: List[str]) -> bool:
+        if input_word in entered_words:
+            print(f'\nWARNING : {input_word} already entered once!!!')
+            return False
+        else:
+            return True
 
+    # check if entered word is of length 5
 
-def checkWordUsed(input_word: str, entered_words: List[str]) -> bool:
-    if input_word in entered_words:
-        print(f'\nWARNING : {input_word} already entered once!!!')
-        return False
-    else:
-        return True
-
-
-# check if entered word is of length 5
-def checkWordLength(input_word: str) -> bool:
-    if len(input_word.strip()) != 5:  # To handle empty spaces strings like "     "
-        print('\nWARNING : Word should be of length 5')
-        return False
-    else:
-        return True
+    def checkWordLength(input_word: str) -> bool:
+        if len(input_word.strip()) != 5:  # To handle empty spaces strings like "     "
+            print('\nWARNING : Word should be of length 5')
+            return False
+        else:
+            return True
--- Wordle.py
+++ Wordle.py
@@ -54,135 +54,141 @@ while attempt < 6
 # ********************************************************** SOURCE-CODE *******************************************************************
 
 from decimal import DivisionByZero
-from UI import *
-from Dictionary import *
-from calculateStatistics import *
-from typing import List
+from UI import Ui
+import Dictionary as dictionaryClass
+from calculateStatistics import CalculateStatistics
+from typing import Dict, List, Tuple
 
 # Initialise all variables
 
 
-def initVariables(words_used: List[str]) -> Tuple[str, List, List, str, int, List]:
-    todays_word, word_list = getRandomWord(words_used)
-    words_used.append(todays_word)
-    entered_words = []
-    answer = [None, None, None, None, None]
-    attempt = 0
-    print(todays_word)
-    return todays_word, word_list, entered_words, answer, attempt, words_used
+class Wordle:
+    def __init__(self) -> None:
+        self.dictionary = dictionaryClass.Dictionary()
 
+    def initVariables(self, words_used: List[str]) -> Tuple[str, List, List, str, int, List]:
+        todays_word, word_list = self.dictionary.getRandomWord(words_used)
+        # print(str(self.dictionary))
+        words_used.append(todays_word)
+        entered_words = []
+        answer = [None, None, None, None, None]
+        attempt = 0
+        # print(todays_word)
+        return todays_word, word_list, entered_words, answer, attempt, words_used
 
-def main():
+    def main(self):
 
-    words_used = []
-    todays_word, word_list, entered_words, answer, attempt, words_used = initVariables(
-        words_used)
-    games_played = 0
-    games_won = 0
-    guess_history = []
+        words_used = []
+        todays_word, word_list, entered_words, answer, attempt, words_used = self.initVariables(
+            words_used)
+        games_played = 0
+        games_won = 0
+        guess_history = []
 
-    while attempt < 6:
-        input_word = inputFromUser(attempt)
+        while attempt < 6:
+            input_word = Ui.inputFromUser(attempt)
 
-        # if empty word entered then quit playing
-        if(len(input_word.strip()) == 0):
-            print("\nThank you for Playing.!")
-            print("\nExiting")
-            displayResults(games_played, games_won, guess_history)
-            break
+            # if empty word entered then quit playing
+            if(len(input_word.strip()) == 0):
+                print("\nThank you for Playing.!")
+                print("\nExiting")
+                self.displayResults(games_played, games_won, guess_history)
+                break
 
-        # check if entered word is having any special characters
-        if(not checkInput(input_word)):
-            continue
+            # check if entered word is having any special characters
+            if(not Ui.checkInput(input_word)):
+                continue
 
-        # check if entered word has already been entered before
-        if(not checkWordUsed(input_word, entered_words)):
-            continue
+            # check if entered word has already been entered before
+            if(not Ui.checkWordUsed(input_word, entered_words)):
+                continue
 
-        # check if entered word is of length 5
-        if(not checkWordLength(input_word)):
-            continue
+            # check if entered word is of length 5
+            if(not Ui.checkWordLength(input_word)):
+                continue
 
-        # check if word was entered previously
-        if(input_word not in word_list):
-            print("\nWARNING : Entered word not in dictionary!")
-            continue
+            # check if word was entered previously
+            if(input_word not in word_list):
+                print("\nWARNING : Entered word not in dictionary!")
+                continue
 
-        # check if the guess is correct
-        if input_word == todays_word:
-            print(" Congrats! You have correctly guessed today's word!",
-                  '\n Thank you for Playing!!')
-            games_played += 1
-            games_won += 1
-            guess_history.append(attempt+1)
-            displayResults(games_played, games_won, guess_history)
-            todays_word, word_list, entered_words, answer, attempt, words_used = initVariables(
-                words_used)
-            print("Starting a New Game, Press enter to exit")
-            continue
+            # check if the guess is correct
+            if input_word == todays_word:
+                print(" Congrats! You have correctly guessed today's word!",
+                      '\n Thank you for Playing!!')
+                games_played += 1
+                games_won += 1
+                guess_history.append(attempt+1)
+                self.displayResults(games_played, games_won, guess_history)
+                todays_word, word_list, entered_words, answer, attempt, words_used = self.initVariables(
+                    words_used)
+                print("Starting a New Game, Press enter to exit")
+                continue
 
-        letter_counts = {}
+            letter_counts = {}
 
-        # Generate a dictionary to store letter count
-        for letter in todays_word:
-            if letter in letter_counts.keys():
-                letter_counts[letter] += 1
-            else:
-                letter_counts[letter] = 1
+            # Generate a dictionary to store letter count
+            for letter in todays_word:
+                if letter in letter_counts.keys():
+                    letter_counts[letter] += 1
+                else:
+                    letter_counts[letter] = 1
 
-        # comparing the characters of input_word and todays_word in corresponding position
-        for index in range(len(input_word)):
-            if input_word[index] == todays_word[index]:
-                answer[index] = todays_word[index]
-                letter_counts[todays_word[index]] -= 1
-            else:
-                answer[index] = '"'
+            # comparing the characters of input_word and todays_word in corresponding position
+            for index in range(len(input_word)):
+                if input_word[index] == todays_word[index]:
+                    answer[index] = todays_word[index]
+                    letter_counts[todays_word[index]] -= 1
+                else:
+                    answer[index] = '"'
 
-        # checking which characters of input_word are present and absent in todays_word
-        for index in range(len(input_word)):
-            if input_word[index] != todays_word[index]:
-                if input_word[index] in letter_counts:
-                    if letter_counts[input_word[index]] > 0:
-                        letter_counts[input_word[index]] -= 1
-                        answer[index] = "`"
+            # checking which characters of input_word are present and absent in todays_word
+            for index in range(len(input_word)):
+                if input_word[index] != todays_word[index]:
+                    if input_word[index] in letter_counts:
+                        if letter_counts[input_word[index]] > 0:
+                            letter_counts[input_word[index]] -= 1
+                            answer[index] = "`"
 
-        entered_words.append(input_word)
-        attempt += 1
-        print(answer)
+            entered_words.append(input_word)
+            attempt += 1
+            print(answer)
 
-        if attempt == 6:
-            print("\nUnfortunately, you were unable to guess today's word")
-            print(f"Today's word was {todays_word}")
-            games_played += 1
-            guess_history.append("Unsuccessful")
-            displayResults(games_played, games_won, guess_history)
-            todays_word, word_list, entered_words, answer, attempt, words_used = initVariables(
-                words_used)
-            print("Starting a New Game, Press enter to exit")
+            if attempt == 6:
+                print("\nUnfortunately, you were unable to guess today's word")
+                print(f"Today's word was {todays_word}")
+                games_played += 1
+                guess_history.append("Unsuccessful")
+                self.displayResults(games_played, games_won, guess_history)
+                todays_word, word_list, entered_words, answer, attempt, words_used = self.initVariables(
+                    words_used)
+                print("Starting a New Game, Press enter to exit")
 
+    def displayResults(self, games_played: int, games_won: int, guess_history: List[str]) -> None:
+        print(f"\nNumber of Games Played : {games_played}")
+        print(f"Guess distribution : {guess_history}")
 
-def displayResults(games_played: int, games_won: int, guess_history: List[str]) -> None:
-    print(f"\nNumber of Games Played : {games_played}")
-    print(f"Guess distribution : {guess_history}")
+        try:
+            print(f"Win % : {(games_won/games_played)*100}")
+        except ZeroDivisionError:
+            print('Win % : 0')
 
-    try:
-        print(f"Win % : {(games_won/games_played)*100}")
-    except ZeroDivisionError:
-        print('Win % : 0')
-
-    try:
-        f = open("gameplay.log", "a")
-        f.write((f"Number of Games Played : {games_played}\n"))
-        f.write(f"Guess distribution : {guess_history}\n")
-        f.write(f"Win % : {(games_won/games_played)*100}\n\n")
-    except IOError:
-        print('An error occured trying to read the file.')
-        print('Please make sure "gameplay.log" is present in the directory before running the program')
-        quit()
-    except ZeroDivisionError:
-        f.write(f"Win % : 0\n\n")
+        try:
+            f = open("gameplay.log", "a")
+            f.write((f"Number of Games Played : {games_played}\n"))
+            f.write(f"Guess distribution : {guess_history}\n")
+            f.write(f"Win % : {(games_won/games_played)*100}\n\n")
+        except IOError:
+            print('An error occured trying to read the file.')
+            print(
+                'Please make sure "gameplay.log" is present in the directory before running the program')
+            quit()
+        except ZeroDivisionError:
+            f.write(f"Win % : 0\n\n")
 
 
 if __name__ == "__main__":
-    main()
-    calculateAndSortWords()
+    game = Wordle()
+    game.main()
+    stats = CalculateStatistics()
+    stats.calculateAndSortWords()
